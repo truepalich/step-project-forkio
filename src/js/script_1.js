4670706b9172c81a3435e443c39cb6c68f2ab739
Ruslan <opryshko.r@gmail.com>
2019-03-18 23:15:55 +0200

    fixed dropmenu

--- src/js/script_1.js
+++ src/js/script_1.js
@@ -3,26 +3,18 @@
 "use strict"
 let btn_open = document.querySelector(".hamburger__btn_open");
 let btn_close = document.querySelector(".hamburger__btn_close");
-let menu = document.querySelector("menu");
-btn_open.addEventListener('mouseover', function() {
+let menu = document.querySelector(".menu");
+
+btn_open.addEventListener('click', function() {
     btn_open.style.display = "none";
+    menu.classList.add('dropmenu');
     btn_close.style.display = "block";
-    menu.style.display = "block";
-    menu.addEventListener('mouseover', () => {
-        btn_open.style.display = "none";
-        menu.style.display = "block";
-        btn_close.style.display = "block";
-    })
+
 })
 
-btn_close.addEventListener("mouseout", function() {
+btn_close.addEventListener("click", function() {
+    menu.classList.remove('dropmenu');
+    menu.classList.add('hidemenu');
     btn_open.style.display = "block";
     btn_close.style.display = "none";
-    menu.style.display = "none";
-    menu.addEventListener('mouseout', () => {
-        btn_open.style.display = "block";
-        btn_close.style.display = "none";
-        menu.style.display = "none";
-    })
-
 });
