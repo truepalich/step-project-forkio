cfaee2ec7feaaf8b6010aadd18dc32d54b47a12d
Ruslan <opryshko.r@gmail.com>
2019-03-18 21:42:49 +0200

    dropmenu on hover

--- src/js/script_1.js
+++ src/js/script_1.js
@@ -4,15 +4,25 @@
 let btn_open = document.querySelector(".hamburger__btn_open");
 let btn_close = document.querySelector(".hamburger__btn_close");
 let menu = document.querySelector("menu");
-btn_open.addEventListener('click', function() {
+btn_open.addEventListener('mouseover', function() {
     btn_open.style.display = "none";
     btn_close.style.display = "block";
     menu.style.display = "block";
+    menu.addEventListener('mouseover', () => {
+        btn_open.style.display = "none";
+        menu.style.display = "block";
+        btn_close.style.display = "block";
+    })
 })
 
-btn_close.addEventListener("click", function() {
+btn_close.addEventListener("mouseout", function() {
     btn_open.style.display = "block";
     btn_close.style.display = "none";
     menu.style.display = "none";
+    menu.addEventListener('mouseout', () => {
+        btn_open.style.display = "block";
+        btn_close.style.display = "none";
+        menu.style.display = "none";
+    })
 
 });
